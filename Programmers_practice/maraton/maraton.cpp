4bab228a365d969ded939dcd4092839888f1530c
Usofl <songjang33@naver.com>
2021-12-27 15:33:47 +0900

    maraton 중간 커밋

--- Programmers_practice/maraton/maraton.cpp
+++ Programmers_practice/maraton/maraton.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
 
-	for (auto iter = completion.begin(); iter != completion.end(); ++iter)
-	{
-		for (auto iter2 = participant.begin(); iter2 != participant.end(); ++iter2)
-		{
-			if (*iter == *iter2)
-			{
-				participant.erase(iter2);
-				break;
-			}
-		}
-	}
+    for (int i = 0; i < completion.size(); ++i) {
+        auto it = find(participant.begin(), participant.end(), completion[i]);
 
-	answer = participant[0];
+        if (it != participant.end())
+        {
+            participant.erase(it);
+            continue;
+        }
+    }
+
+    answer = participant[0];
 
     return answer;
 }
